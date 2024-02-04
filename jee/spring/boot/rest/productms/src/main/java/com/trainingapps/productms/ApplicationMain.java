12422c16c8843d6e3802287576035b54db8b518e
yash bakadiya <you@example.comyash.bakadiya1@wipro.com>
2024-02-04 11:09:48 +0530

    exploring things

--- jee/spring/boot/rest/productms/src/main/java/com/trainingapps/productms/ApplicationMain.java
+++ jee/spring/boot/rest/productms/src/main/java/com/trainingapps/productms/ApplicationMain.java
@@ -2,6 +2,8 @@ package com.trainingapps.productms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.logging.Logger;
+
 // import org.springframework.context.annotation.Bean;
 // import org.springframework.web.cors.CorsConfiguration;
 // import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -10,8 +12,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApplicationMain {
 
+    private static final Logger logger = Logger.getLogger(ApplicationMain.class.getName());
+
     public static void main(String args[]){
        SpringApplication.run(ApplicationMain.class,args);
+       logger.info("Application started");
 
     }
     // @Bean
