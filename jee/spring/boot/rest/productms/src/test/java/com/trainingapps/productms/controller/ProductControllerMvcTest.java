1ad3c305f3158b37c7d3816a4c9ad1d3d059d151
yash bakadiya <you@example.comyash.bakadiya1@wipro.com>
2024-07-04 22:58:01 +0530

    Last changes

--- jee/spring/boot/rest/productms/src/test/java/com/trainingapps/productms/controller/ProductControllerMvcTest.java
+++ jee/spring/boot/rest/productms/src/test/java/com/trainingapps/productms/controller/ProductControllerMvcTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -66,6 +67,5 @@ class ProductControllerMvcTest {
                 .andExpect(content().string(expectedResponse))
         ;
     }
-
 
 }
